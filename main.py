31eedaf5dcca6c5022f218616875d173460306e3
김한별 <anarchytoast@gimhanbyeol-ui-MacBookAir.local>
2025-03-14 14:55:49 +0900

    changed stuff

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+import numpy as np
+import matplotlib.pylab as plt
+import matplotlib
+
+x = np.linspace(0, 2*np.pi, 100)
+plt.plot(x, np.sin(x))
+plt.title("Look, a Sine function")
+plt.show()
